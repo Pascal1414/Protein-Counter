44b2a48816df33914fb76387100d5a2cef81882a
Pascal <pascal.rieder@outlook.com>
2023-11-26 21:29:49 +0100

    Update ItemsView

--- app/src/main/java/com/pascalrieder/proteincounter/ui/ItemsView.kt
+++ app/src/main/java/com/pascalrieder/proteincounter/ui/ItemsView.kt
@@ -1,9 +1,19 @@
 package com.pascalrieder.proteincounter.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.pascalrieder.proteincounter.data.DataProvider
 
 @Composable
 fun ItemsView() {
-    Text(text = "Items")
+    Column(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
+        DataProvider.getItems().forEach {
+            Text(text = it.name + " (" + it.proteinContentPercentage + "%)")
+        }
+    }
 }
