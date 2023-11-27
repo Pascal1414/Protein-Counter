fcff5aec678eb1a19f4f632c6a987de4d331a8f2
Pascal <pascal.rieder@outlook.com>
2023-11-27 13:29:38 +0100

    Revert "Update Theme"
    This reverts commit 6cb420a0737576a3d79c8b6eae48beb93a94a3ea.

--- app/src/main/java/com/pascalrieder/proteincounter/ui/theme/Color.kt
+++ app/src/main/java/com/pascalrieder/proteincounter/ui/theme/Color.kt
@@ -1,11 +1,11 @@
 package com.pascalrieder.proteincounter.ui.theme
 
 import androidx.compose.ui.graphics.Color
-// https://m2.material.io/design/material-studies/rally.html
-val RallyGreen = Color(0xFF1EB980)
-val RallyDarkGreen = Color(0xFF045D56)
-val RallyOrange = Color(0xFFFF6859)
-val RallyYellow = Color(0xFFFFCF44)
-val RallyPurple = Color(0xFFB15DFF)
-val RallyBlue = Color(0xFF72DEFF)
-val White = Color(0xFFFFFFFF)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
--- app/src/main/java/com/pascalrieder/proteincounter/ui/theme/Theme.kt
+++ app/src/main/java/com/pascalrieder/proteincounter/ui/theme/Theme.kt
@@ -10,44 +10,31 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    // Primary colors
-    primary = RallyGreen,
-    onPrimary = Color.White,
-    primaryContainer = Color(0xFF61CEA4),
-    onPrimaryContainer = Color(0xFF306650),
-
-    // Secondary colors
-    secondary = RallyDarkGreen,
-    onSecondary = Color.White,
-    secondaryContainer = Color(0xFF08A396),
-    onSecondaryContainer = Color(0xFF034942),
-
-    // Tertiary colors
-    tertiary = RallyOrange,
-    onTertiary = Color.White,
-    tertiaryContainer = Color(0xFFFF8B7D),
-    onTertiaryContainer = Color(0xFFD83220),
-
-
-    // Error colors
-    error = Color.Red,
-
-    // Background and Surface colors
-    background = Color(0xFF373740),
-    onBackground = Color.White,
-    surface = Color(0xFF373740),
-    onSurface = Color.White
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
 
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
 )
 
 @Composable
