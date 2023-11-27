6cb420a0737576a3d79c8b6eae48beb93a94a3ea
Pascal <pascal.rieder@outlook.com>
2023-11-27 12:49:35 +0100

    Update Theme

--- app/src/main/java/com/pascalrieder/proteincounter/ui/theme/Type.kt
+++ app/src/main/java/com/pascalrieder/proteincounter/ui/theme/Type.kt
@@ -2,33 +2,71 @@ package com.pascalrieder.proteincounter.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.pascalrieder.proteincounter.R
 
-// Set of Material typography styles to start with
+private val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(
+            Font(googleFont = GoogleFont("Eczar"), fontProvider = provider)
+        ),
+        fontSize = 48.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily(
+            Font(googleFont = GoogleFont("Eczar"), fontProvider = provider)
+        ),
+        fontSize = 36.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily(
+            Font(googleFont = GoogleFont("Eczar"), fontProvider = provider)
+        ),
+        fontSize = 24.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily(
+            Font(googleFont = GoogleFont("Eczar"), fontProvider = provider)
+        ),
+        fontSize = 20.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily(
+            Font(googleFont = GoogleFont("Eczar"), fontProvider = provider)
+        ),
+        fontSize = 18.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontFamily = FontFamily(
+            Font(googleFont = GoogleFont("Eczar"), fontProvider = provider)
+        ),
+        fontSize = 16.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily(
+            Font(googleFont = GoogleFont("Roboto Condensed"), fontProvider = provider)
+        ),
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(
+            Font(googleFont = GoogleFont("Roboto Condensed"), fontProvider = provider)
+        ),
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(
+            Font(googleFont = GoogleFont("Roboto Condensed"), fontProvider = provider)
+        ),
+        fontSize = 12.sp
+    ),
 )
