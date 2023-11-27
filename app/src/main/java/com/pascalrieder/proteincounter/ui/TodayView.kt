43fa2859cc7851cd06003591e7c8e70aa228ed81
Pascal <pascal.rieder@outlook.com>
2023-11-27 17:25:34 +0100

    Update TodayView

--- app/src/main/java/com/pascalrieder/proteincounter/ui/TodayView.kt
+++ app/src/main/java/com/pascalrieder/proteincounter/ui/TodayView.kt
@@ -1,7 +1,9 @@
 package com.pascalrieder.proteincounter.ui
 
 import android.os.Handler
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,16 +11,14 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pascalrieder.proteincounter.data.DataProvider
 import com.pascalrieder.proteincounter.data.Item
@@ -231,64 +231,85 @@ fun TodayView() {
 }
 
 @Composable
-fun ItemView(item: Item) {
-    Column(
+fun ItemView(item: Item ) {
+    var isExpanded by remember { mutableStateOf(false) }
+
+    Row(
         modifier = Modifier.fillMaxWidth()
-            .background(MaterialTheme.colorScheme.primaryContainer, RoundedCornerShape(15)).padding(24.dp)
+            .animateContentSize()
+            .height(if (isExpanded) 170.dp else 100.dp)
+            .background(MaterialTheme.colorScheme.primaryContainer, RoundedCornerShape(15)).padding(24.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(
-            style = MaterialTheme.typography.headlineSmall,
-            text = item.name
-        )
-        Text(
-            style = MaterialTheme.typography.bodyMedium,
-            text = item.amountInGramm.toString() + "g ",
-            modifier = Modifier.alpha(0.5f)
-        )
-        Spacer(modifier = Modifier.height(40.dp))
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
+        Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.SpaceBetween) {
+            Column {
+                Text(
+                    style = MaterialTheme.typography.headlineSmall,
+                    text = item.name
+                )
                 Text(
                     style = MaterialTheme.typography.bodyMedium,
-                    text = String.format("%.0f", item.amountInGramm) + "g",
+                    text = item.amountInGramm.toString() + "g ",
+                    modifier = Modifier.alpha(0.5f)
                 )
-                Spacer(modifier = Modifier.width(10.dp))
-                Text(
-                    style = MaterialTheme.typography.bodyMedium,
-                    text = "×",
-                )
-                Spacer(modifier = Modifier.width(10.dp))
-                Column(modifier = Modifier.wrapContentSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            }
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
                     Text(
                         style = MaterialTheme.typography.bodyMedium,
-                        text = String.format("%.1f", item.proteinContentPercentage).replace(".0", "") + "g"
+                        text = String.format("%.0f", item.amountInGramm) + "g",
                     )
-                    Divider(
-                        color = MaterialTheme.colorScheme.primary,
-                        thickness = 1.dp,
-                        modifier = Modifier.width(30.dp).padding(top = 3.dp)
-                    )
+                    Spacer(modifier = Modifier.width(10.dp))
                     Text(
                         style = MaterialTheme.typography.bodyMedium,
-                        text = "100g",
+                        text = "×",
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Column(
+                        modifier = Modifier.wrapContentSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            style = MaterialTheme.typography.bodyMedium,
+                            text = String.format("%.1f", item.proteinContentPercentage).replace(".0", "") + "g"
+                        )
+                        Divider(
+                            color = MaterialTheme.colorScheme.primary,
+                            thickness = 1.dp,
+                            modifier = Modifier.width(30.dp).padding(top = 3.dp)
+                        )
+                        Text(
+                            style = MaterialTheme.typography.bodyMedium,
+                            text = "100g",
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        style = MaterialTheme.typography.bodyMedium,
+                        text = "="
                     )
                 }
-                Spacer(modifier = Modifier.width(10.dp))
-                Text(
-                        style = MaterialTheme.typography.bodyMedium,
-                text = "="
+            }
+        }
+        Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxHeight()) {
+            IconButton(onClick = { isExpanded = !isExpanded }, modifier = Modifier.align(Alignment.End).size(15.dp)) {
+                Icon(
+                    imageVector = if (isExpanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Info Icon",
+                    modifier = Modifier.fillMaxSize()
                 )
             }
 
             Text(
                 style = MaterialTheme.typography.titleLarge,
-                text = String.format("%.1f", (item.amountInGramm * item.proteinContentPercentage / 100)).replace(".0", "")  + "g"
+                text = String.format("%.1f", (item.amountInGramm * item.proteinContentPercentage / 100))
+                    .replace(".0", "") + "g"
             )
         }
+
     }
 }
 
