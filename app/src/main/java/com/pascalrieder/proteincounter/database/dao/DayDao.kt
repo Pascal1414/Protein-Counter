d38b1ffaa675676af46d61d337aa2b172c24740d
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-01-22 18:00:09 +0100

    Update DayDao

--- app/src/main/java/com/pascalrieder/proteincounter/database/dao/DayDao.kt
+++ app/src/main/java/com/pascalrieder/proteincounter/database/dao/DayDao.kt
@@ -6,18 +6,17 @@ import androidx.room.Insert
 import androidx.room.Query
 import com.pascalrieder.proteincounter.database.dto.DayWithItemsDb
 import com.pascalrieder.proteincounter.database.models.Day
-import com.pascalrieder.proteincounter.database.models.DayItem
 import java.time.LocalDate
 
 @Dao
 interface DayDao {
     @Query(
-        "SELECT Day.uid AS dayId, Day.date, Item.uid AS itemId, Item.name, Item.proteinContentPercentage, Item.kcalContentIn100g, DayItem.amountInGram FROM Day LEFT JOIN DayItem ON Day.uid = DayItem.dayId LEFT JOIN Item ON DayItem.itemId = Item.uid;"
+        "SELECT Day.uid AS dayId, Day.date, Item.uid AS itemId, Item.name, Item.proteinContentPercentage, Item.kcalContentIn100g, DayItem.amountInGram FROM Day LEFT JOIN DayItem ON Day.uid = DayItem.dayId LEFT JOIN Item ON DayItem.itemId = Item.uid WHERE DayItem.isDeleted = 0;"
     )
     fun readAllData(): LiveData<List<DayWithItemsDb>>
 
     @Query(
-        "SELECT Day.uid AS dayId, Day.date, Item.uid AS itemId, Item.name, Item.proteinContentPercentage, Item.kcalContentIn100g, DayItem.amountInGram FROM Day LEFT JOIN DayItem ON Day.uid = DayItem.dayId LEFT JOIN Item ON DayItem.itemId = Item.uid WHERE Day.date = :date;"
+        "SELECT Day.uid AS dayId, Day.date, Item.uid AS itemId, Item.name, Item.proteinContentPercentage, Item.kcalContentIn100g, DayItem.amountInGram FROM Day LEFT JOIN DayItem ON Day.uid = DayItem.dayId LEFT JOIN Item ON DayItem.itemId = Item.uid WHERE Day.date = :date AND DayItem.isDeleted = 0;"
     )
     fun readDayEntriesFromDate(date: LocalDate): LiveData<List<DayWithItemsDb>>
 
@@ -26,6 +25,6 @@ interface DayDao {
     @Query("UPDATE DAYITEM SET isDeleted = 1 WHERE dayId = :dayId AND itemId = :itemId")
     fun removeItemFromDay(dayId: Long, itemId: Long)
 
-    @Query("Insert Into DayItem (dayId, itemId, amountInGram, isDeleted) values (:dayId, :itemId, :amount, :isDeleted)")
+    @Query("Insert Into DayItem (dayId, itemId, amountInGram, isDeleted) values (:dayId, :itemId, :amount, :isDeleted);")
     fun addItemToDay(dayId: Long, itemId: Long, amount: Float, isDeleted: Boolean)
 }
